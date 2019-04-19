c9486a9aa2ff6b3ae86c3dd31ee433c5d02c43d1
SakuraRhymes <sakurarhymes@gmail.com>
2019-04-19 18:43:22 +0800

    Update button position

--- Assignment/javascript/solar.js
+++ Assignment/javascript/solar.js
@@ -1,3 +1,33 @@
 function onHoverChangeiframe(planetName) {
-    document.querySelector("#iframe_planet").src = planetName + ".html";
+    var iframe = document.querySelector("#iframe_planet");
+    iframe.src = planetName + ".html";
+    switch (planetName) {
+        case "sun":
+            iframe.style.left = "30%";
+            break;
+        case "mercury":
+            iframe.style.left = "30%";
+            break;
+        case "venus":
+            iframe.style.left = "35%";
+            break;
+        case "earth":
+            iframe.style.left = "40%";
+            break;
+        case "mars":
+            iframe.style.left = "45%";
+            break;
+        case "jupiter":
+            iframe.style.left = "63%";
+            break;
+        case "saturn":
+            iframe.style.left = "21%";
+            break;
+        case "uranus":
+            iframe.style.left = "35%";
+            break;
+        case "neptune":
+            iframe.style.left = "40%";
+            break;
+    }
 }

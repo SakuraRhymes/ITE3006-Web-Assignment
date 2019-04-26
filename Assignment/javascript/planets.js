bc8d444596da44a09d7cff8428aa153fbab906cd
SakuraRhymes <sakurarhymes@gmail.com>
2019-04-26 15:47:05 +0800

    Updated earth

--- Assignment/javascript/planets.js
+++ Assignment/javascript/planets.js
@@ -1,12 +1,13 @@
 var photoIndex = 0;
 
-function nextPhoto() {
-    photoIndex++;
+function nextPhoto(direction) {
+    photoIndex += direction;
     var photo = document.getElementsByClassName("photo");
+    if (photoIndex < 0)
+        photoIndex = (photo.length - 1);
     photoIndex = photoIndex % photo.length;
     for (i = 0; i < photo.length; i++) {
         photo[i].style.display = "none";
     }
     photo[photoIndex].style.display = "block";
-
 }
